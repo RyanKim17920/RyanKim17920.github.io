fa971df55086656f4b5aee4132de4609f14014ef
RyanKim17920 <ryankim17920@gmail.com>
2026-04-14 03:40:09 +0000

    Deploying to gh-pages from @ RyanKim17920/RyanKim17920.github.io@79c1e325f85e0239a7ce39206ff2c0a4867e6146 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,14 +18,14 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "Open-source and full-stack work. All code on GitHub.",
+          description: "Open-source and full-stack work. All code on GitHub at github.com/RyanKim17920.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
